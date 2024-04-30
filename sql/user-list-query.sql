1bef366d82597476cd187440024227f4005c1aef
Vinfall <91039000+Vinfall@users.noreply.github.com>
2024-04-30 15:54:10 +0800

    sql: test query

---
+++ sql/user-list-query.sql
@@ -0,0 +1,35 @@
+select vn.title,
+    vn.alias,
+    round(CAST(vote AS numeric) / 10, 1) as "Vote",
+    u.vid,
+    u.started,
+    u.finished,
+    round(avg(lv.length) / 60, 2) as "Length",
+    case
+        when min(released) % 100 = 99 then min(released) - 98
+        else min(released)
+    end as "Release Date",
+    -- Deal with 99999901 stuff
+    c_votecount as "RatingDP",
+    round(CAST(c_rating AS numeric) / 100, 2) as "Rating",
+    labels
+from vndb.ulist_vns u
+    join vndb.vn vn on u.vid = vn.id
+    left join releases_vn rvn on rvn.vid = vn.id
+    left join releases r on r.id = rvn.id
+    and rtype <> 'trial'
+    left join vn_length_votes lv on vn.id = lv.vid
+    and speed = 1
+WHERE u.uid = { UID } --Change to your User ID
+    -- and vote is not null  -- Add this if you want to only Voted VN
+    AND NOT labels @> '{5}' -- Exclude wishlist items (label value == {5})
+group by vn.title,
+    vn.alias,
+    vote,
+    u.vid,
+    u.vid,
+    u.started,
+    u.finished,
+    c_votecount,
+    c_rating,
+    labels
