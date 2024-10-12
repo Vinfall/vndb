ef960670d9c91023c08a3d16c747330425c9e068
Vinfall <91039000+Vinfall@users.noreply.github.com>
2024-10-12 20:57:29 +0800

    fix: sql format

--- sql/lengthvotes-query.sql
+++ sql/lengthvotes-query.sql
@@ -6,7 +6,7 @@ SELECT TO_CHAR(lv.date, 'YYYY-MM-DD') AS date,
         WHEN lv.speed = 0 THEN 'Slow'
         WHEN lv.speed = 1 THEN 'Normal'
         WHEN lv.speed = 2 THEN 'Fast'
-        WHEN lv.speed is NULL THEN '-'
+        WHEN lv.speed is NULL THEN ''
     END as "speed",
     ARRAY_TO_STRING(lv.rid, ',') AS rid,
     lv.notes
