1f0ec59dc5ac74b933882bd621d3754ca39f8721
Vinfall <91039000+Vinfall@users.noreply.github.com>
2024-10-13 15:44:05 +0800

    feat: multiple query support

--- vndb_query.py
+++ vndb_query.py
@@ -7,7 +7,6 @@ import urllib.request
 
 # Output files
 _OUTPUT_FOLDER = "output/"
-_CSV_FILE = "lengthvotes.csv"
 
 
 # Edited from first-fm
@@ -25,17 +24,23 @@ def load_secrets():
     return doc
 
 
+def get_data(id, output):
+    # Get query results
+    url = "https://query.vndb.org/" + id + ".csv"
+    # trunk-ignore(bandit/B310)
+    with urllib.request.urlopen(url, timeout=30) as response:
+        data = response.read().decode("utf-8").splitlines()
+
+    os.makedirs(_OUTPUT_FOLDER, exist_ok=True)
+    with open(_OUTPUT_FOLDER + output, "w", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        for row in csv.reader(data):
+            writer.writerow(row)
+
+
 secrets = load_secrets()
 lvid = secrets["LVID"]
+mmid = secrets["MMID"]
 
-# Get query results
-url = "https://query.vndb.org/" + lvid + ".csv"
-# trunk-ignore(bandit/B310)
-with urllib.request.urlopen(url, timeout=30) as response:
-    lengthvotes = response.read().decode("utf-8").splitlines()
-
-os.makedirs(_OUTPUT_FOLDER, exist_ok=True)
-with open(_OUTPUT_FOLDER + _CSV_FILE, "w", encoding="utf-8") as file:
-    writer = csv.writer(file)
-    for row in csv.reader(lengthvotes):
-        writer.writerow(row)
+get_data(lvid, "lengthvotes.csv")
+get_data(mmid, "monthly.csv")
