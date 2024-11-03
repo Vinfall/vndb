541af894ae42e277b007448d47893e5e3f52c4c1
Vinfall <91039000+Vinfall@users.noreply.github.com>
2024-11-03 20:29:44 +0800

    chore: sync w/ monthly.sql

--- sql/user-list.sql
+++ sql/user-list.sql
@@ -1,23 +1,22 @@
-SELECT vn.title,
-    string_agg(distinct p.name, ',') AS "developer",
-    round(CAST(vote AS numeric) / 10, 1) AS "vote",
-    u.started,
-    u.finished,
-    CASE
-        min(released) %100
-        WHEN 99 THEN min(released) -98
-        ELSE min(released)
-    END AS "released",
-    -- deal with 99999901 stuff
-    c_votecount AS "ratingDP",
+SELECT round(CAST(vote AS numeric) / 10, 1) AS "vote",
     round(CAST(c_rating AS numeric) / 100, 2) AS "rating",
+    c_votecount AS "ratingDP",
     CASE
         WHEN labels @> '{1}' THEN 'Playing' -- possible to have multiple labels
         WHEN labels @> '{2}' THEN 'Finished'
         WHEN labels @> '{3}' THEN 'Stalled'
         WHEN labels @> '{4}' THEN 'Dropped'
         WHEN labels @> '{5}' THEN 'Wishlist'
-    END AS "labels"
+    END AS "labels",
+    vn.title,
+    string_agg(distinct p.name, ',') AS "developer",
+    u.started,
+    u.finished,
+    CASE
+        min(released) %100
+        WHEN 99 THEN min(released) -98
+        ELSE min(released)
+    END AS "released" -- deal with 99999901 stuff
 FROM vndb.ulist_vns u
     JOIN vndb.vn vn ON u.vid = vn.id
     LEFT JOIN releases_vn rvn ON rvn.vid = vn.id
@@ -36,4 +35,7 @@ GROUP BY vn.title,
     c_votecount,
     c_rating,
     labels
-ORDER BY u.started DESC
+ORDER BY labels DESC,
+    -- finished > dropped
+    u.finished DESC,
+    u.started DESC
