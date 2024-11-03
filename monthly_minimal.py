6034426cd204bc9461ee1cb49841a71cf204e1a7
Vinfall <91039000+Vinfall@users.noreply.github.com>
2024-11-03 19:48:44 +0800

    fix: redundant where clause as monthly.sql parsed it already

--- monthly_minimal.py
+++ monthly_minimal.py
@@ -11,27 +11,10 @@ OUTPUT_PATH = "output/monthly-minimal.csv"
 QUERY = "sql/monthly-minimal.sql"
 
 
-def get_last_month_dates():
-    # Get the current date
-    today = pd.to_datetime("today").normalize()  # noqa
-
-    # Get the start and end dates of last month
-    month_start = (today - pd.offsets.MonthBegin(2)).strftime("%Y-%m-%d")
-    month_end = (today - pd.offsets.MonthEnd(1)).strftime("%Y-%m-%d")
-
-    return month_start, month_end
-
-
 def query_csv(input_csv, output_csv, sql_query):
     # Read query from file
     with open(sql_query, "r", encoding="utf-8") as file:
         query = file.read()
-
-    # Replace date on demand
-    last_month_start, last_month_end = get_last_month_dates()
-    query = query.replace("2024-10-01", last_month_start).replace(
-        "2024-10-31", last_month_end
-    )
 
     # Create a memory SQLite DB
     conn = sqlite3.connect(":memory:")
@@ -46,10 +29,10 @@ def query_csv(input_csv, output_csv, sql_query):
 
 try:
     with open(INPUT_PATH, "r", encoding="utf-8") as f:
-        pass  # 仅检查文件是否存在
+        pass  # Check if file exist
 except FileNotFoundError:
     print("Monthly list not found. Export via VNDB Query first.")
     sys.exit()
 
-# 调用查询函数
+# Query results
 query_csv(INPUT_PATH, OUTPUT_PATH, QUERY)
