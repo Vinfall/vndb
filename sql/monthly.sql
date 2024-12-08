e4139aa52d1d1aed8e3f41adda566110783609fc
Vinfall <91039000+Vinfall@users.noreply.github.com>
2024-12-08 18:12:44 +0800

    fix/sql: 99999901 error
    dumb conversion, but it works™

--- sql/monthly.sql
+++ sql/monthly.sql
@@ -13,14 +13,9 @@ SELECT round(CAST(vote AS numeric) / 10, 1) AS "vote",
     u.started,
     u.finished,
     CASE
-        WHEN min(released) % 100 = 99 THEN TO_CHAR(
-            TO_DATE((min(released) - 98)::text, 'YYYYMMDD'),
-            'YYYY-MM-DD'
-        ) -- deal with 99999901 stuff
-        ELSE TO_CHAR(
-            TO_DATE(min(released)::text, 'YYYYMMDD'),
-            'YYYY-MM-DD'
-        ) -- convert to ISO date
+        WHEN min(released) IS NULL THEN '2099-12-31'
+        WHEN LENGTH(min(released)::TEXT) = 8 THEN SUBSTR(min(released)::TEXT, 1, 4) || '-' || SUBSTR(min(released)::TEXT, 5, 2) || '-' || SUBSTR(min(released)::TEXT, 7, 2)
+        ELSE '2099-12-31' -- dumb ISO date conversion
     END AS "released"
 FROM vndb.ulist_vns u
     JOIN vndb.vn vn ON u.vid = vn.id
@@ -33,12 +28,12 @@ WHERE u.uid = { UID } -- change to your User ID
     -- and vote is not null  -- voted VN only
     AND NOT labels @> '{5}' -- exclude wishlist
     AND (
-        u.finished BETWEEN '2024-10-01' AND '2024-10-31' -- finished this month
+        u.finished BETWEEN '2024-11-01' AND '2024-11-30' -- finished this month
         OR (
             u.finished IS NULL
-            AND NOT u.started > '2024-10-31' -- WIP/Dropped/Stalled
+            AND NOT u.started > '2024-11-30' -- WIP/Dropped/Stalled
             AND NOT (
-                u.started < '2024-10-01' -- excluded earlier VNs
+                u.started < '2024-11-01' -- excluded earlier VNs
                 AND NOT labels @> '{1}'
             )
         )
